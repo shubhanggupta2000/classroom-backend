c16b7ddba0f4c4f212a09608389f862eec3e778c
Shubhang Gupta <guptashubhang2000@gmail.com>
2026-01-30 19:50:28 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import AgentAPI from "apminsight";
+AgentAPI.config();
+
+import express from "express";
+import cors from "cors";
+
+import subjectsRouter from "./routes/subjects.js";
+import usersRouter from "./routes/users.js";
+import classesRouter from "./routes/classes.js";
+import securityMiddleware from "./middleware/security.js";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
+
+const app = express();
+const PORT = 8000;
+
+if (!process.env.FRONTEND_URL)
+  throw new Error("FRONTEND_URL is not set in .env file");
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.use(express.json());
+
+app.use("/api/subjects", subjectsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/classes", classesRouter);
+
+app.use(securityMiddleware);
+
+app.get("/", (req, res) => {
+  res.send("Hello, welcome to the Classroom API!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
