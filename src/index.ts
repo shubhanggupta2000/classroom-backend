d97713b68a1d1a6bfe733abb0fb781d340e9763b
Shubhang Gupta <guptashubhang2000@gmail.com>
2026-02-01 12:28:02 +0530

    implement security middleware

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 import subjectsRouter from "./routes/subjects.js";
 // import usersRouter from "./routes/users.js";
 // import classesRouter from "./routes/classes.js";
-// import securityMiddleware from "./middleware/security.js";
+import securityMiddleware from "./middleware/security.js";
 // import { toNodeHandler } from "better-auth/node";
 // import { auth } from "./lib/auth.js";
 
@@ -33,7 +33,7 @@ app.use("/api/subjects", subjectsRouter);
 // app.use("/api/users", usersRouter);
 // app.use("/api/classes", classesRouter);
 
-// app.use(securityMiddleware);
+app.use(securityMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Hello, welcome to the Classroom API!");
